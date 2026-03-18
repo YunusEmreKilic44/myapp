b989a4063bd36e3d143a8c174636e977fec3babc
yunusemrkilic44 <yunusemrkilic44@gmail.com>
2026-03-18 05:52:24 +0000

    hatalı kullanıcı bilgilerinde. hata gösterimi

--- lib/DinamikDrawerUygulaması/loginview.dart
+++ lib/DinamikDrawerUygulaması/loginview.dart
@@ -46,12 +46,41 @@ class _LoginViewState extends State<LoginView> {
     );
   }
 
+  void loginPopup() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Hatalı Bilgiler"), // Popup başlığı.
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Kullanıcı Adı veya şifre hatalı",
+              ), // Kullanıcı adını göster.
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () =>
+                  Navigator.of(context).pop(), // Popup'ı kapatma butonu.
+              child: const Text("Tamam"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void loginCheck() {
     String kullaniciAdi = kullaniciAdiKontrolcusu.text;
     String password = parolaKontrolcusu.text;
 
     if (kullaniciAdi == "admin" && password == "123") {
       Navigator.pushNamed(context, '/dinamikDrawer');
+    } else {
+      loginPopup();
     }
   }
 }
